5de8122438f50ea15773ac3efc9a368da6ebf609
huangying-just <huangying-just@126.com>
2024-08-20 15:46:36 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,11 +3,11 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
-  privacy_policy: '',
-  default_language: 'en',
+  title: '智能物流助教',
+  description: '通过与AI助教进行5-10题的问答，给出评价',
+  copyright: '中物联教育培训',
+  privacy_policy: ' AI也会犯错，请注意分辨',
+  default_language: 'zh-Hans',
 }
 
 export const isShowPrompt = false
@@ -18,3 +18,7 @@ export const API_PREFIX = '/api'
 export const LOCALE_COOKIE_NAME = 'locale'
 
 export const DEFAULT_VALUE_MAX_LEN = 48
+
+
+export const isShowPrompt = true
+export const promptTemplate = ''
